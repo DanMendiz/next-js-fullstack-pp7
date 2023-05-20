17e02dd5b477daed35045e22c2c40985bacc4a97
DanMendiz <114294487+DanMendiz@users.noreply.github.com>
2023-05-20 18:54:45 +0100

    mui exports

--- components/mui/index.js
+++ components/mui/index.js
@@ -1,5 +1,21 @@
 import Button from '@mui/material/Button';
 import EditCalendarIcon from '@mui/icons-material/EditCalendar';
 import { Container } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
-export { Button, EditCalendarIcon, Container };
+export {
+  Button,
+  EditCalendarIcon,
+  Container,
+  AppBar,
+  Box,
+  IconButton,
+  MenuIcon,
+  Toolbar,
+  Typography,
+};

df04dfce1d6b3074b7d10ddfb6b3e2e2f0a2c000
DanMendiz <114294487+DanMendiz@users.noreply.github.com>
2023-05-20 14:28:11 +0100

    mui installation

---
+++ components/mui/index.js
@@ -0,0 +1,8 @@
+import Button from '@mui/material/Button';
+import AirIcon from '@mui/icons-material/Air';
+import EditCalendarIcon from '@mui/icons-material/EditCalendar';
+export {
+  Button,
+  AirIcon,
+  EditCalendarIcon
+}

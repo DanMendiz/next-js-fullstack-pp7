4c2c4e4da00de877f86d2716ff63cd5971cd8e18
DanMendiz <114294487+DanMendiz@users.noreply.github.com>
2023-05-30 22:12:53 +0100

    test

---
+++ components/UserDisplay.jsx
@@ -0,0 +1,23 @@
+// import { useUser } from "@auth0/nextjs-auth0/client";
+import Image from "next/image";
+import {
+  Typography,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Avatar,
+  List,
+  // ListItem,
+} from "@/components/mui";
+// import Paragraph from "@/components/Paragraph";
+import Heading from "@/components/Heading";
+
+
+const UserDisplay = ({ssd}) => {
+  return (
+  
+  );
+};
+
+export default UserDisplay;

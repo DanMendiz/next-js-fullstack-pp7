da47083fd8b8a95226f49903a8514d998db4df66
DanMendiz <114294487+DanMendiz@users.noreply.github.com>
2023-05-30 17:46:42 +0100

    change 8

--- components/BasketList.jsx
+++ components/BasketList.jsx
@@ -15,7 +15,7 @@ const ProductList = ({
   const { user } = useUser();
   // const mutation = useAddToBasket();
   const { data: products } = useProducts(); //imported from tanstack query
-  if (!products.length) return <Paragraph>No products to show</Paragraph>;
+  if (!products.length) return <Paragraph>No products* to show</Paragraph>;
   return (
     <List
       component="ol"
